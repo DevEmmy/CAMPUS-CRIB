915ddd2ad22572ac65f3d1ffe195dce1c7e44e66
Black Bobs <ayodejiayodele350@gmail.com>
2025-06-30 19:01:54 +0100

    fix:fixed build error

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import React, { useEffect, useState } from "react";
 import StudentHome from "../StudentHome";
 import AgentHome from "../AgentHome";
 import { useUserStore } from "../../store/UseUserStore";
-// import { useQuery } from "@tanstack/react-query";
-// import { fetchUser } from "../../lib/fetchUser";
-
 const Home: React.FC = () => {
-    // const [accountType, setAccountType] = useState<"AGENT" | "BASIC">("BASIC");
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const [loggedUser, setLoggedUser] = useState<any | null>(null);
 
     const { user  } = useUserStore();
@@ -20,10 +15,10 @@ const Home: React.FC = () => {
 
     useEffect(() => {},[loggedUser])
 
-    // const user = useQuery({ queryKey: ['todos'], queryFn: fetchUser })
+
   return (
     <section>
-      {loggedUser?.userType === "AGENT" ? <AgentHome user={loggedUser} /> : <StudentHome user={loggedUser} />}
+      {loggedUser?.userType === "AGENT" ? <AgentHome user={loggedUser} /> : <StudentHome />}
     </section>
   );
 };
