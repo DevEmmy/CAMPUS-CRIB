48298949dc80e337424df60fbb0b9b60e651821e
DevEmmy <eolaosebikan60@gmail.com>
2025-07-19 11:34:57 +0100

    update: modify ESLint configuration to include src/components/**/* in ignored files

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist', 'src/pages/**/*'] },
+  { ignores: ['dist', 'src/pages/**/*, src/components/**/*'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
